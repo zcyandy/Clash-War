80fd0f1818c6871d20fe90bfaf751a889e4c4943
zcyandy <zcyandy@hotmail.com>
2017-06-11 19:21:56 +0800

    Bone
    Bone Animation

---
+++ Test_6_9.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Test_6_9 : MonoBehaviour {
+
+    public SkinnedMeshRenderer mf;
+
+    public Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
+
+	// Use this for initialization
+	void Start () {
+
+        var mesh = mf.sharedMesh;
+        for (int i = 0; i < mesh.boneWeights.Length; i++)
+        {
+            var weight = mesh.boneWeights[i];
+            if(!dic.ContainsKey(weight.boneIndex0))
+            {
+                dic[weight.boneIndex0] = new List<int>();
+            }
+            dic[weight.boneIndex0].Add(i);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
